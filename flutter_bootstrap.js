98e2e5c8500c079a015cb1792b0f88f8c41ee445
Marc Feldmann <marc@marcsfirma.de>
2026-06-21 21:04:55 +0200

    Anbietersuche live (OSM-Suche)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "377012558" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4223728071" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
